8d3f3f292d7eabeb5e977c8e598874a0042d4cce
teymuruae <baratov.teymur@yandex.ru>
2026-08-15 13:44:18 +0300

    6.2 HTTP API-клиенты сервисов. Практическое задание: HTTP API-клиент для operations-service

---
+++ tests/clients/http/operations/client.py
@@ -0,0 +1,44 @@
+from uuid import UUID
+from httpx import Response
+
+from tests.config import test_settings
+from tests.schema.operations import GetOperationsQueryTestSchema, GetOperationResponseTestSchema, \
+    GetOperationsResponseTestSchema
+from tests.tools.logger import get_test_logger
+from tests.tools.routes import APITestRoutes
+
+from tests.clients.http.client import HTTPTestClient, build_http_test_client
+
+
+class OperationsHttpTestClient(HTTPTestClient):
+    def get_operation_api(self, operation_id: UUID) -> Response:
+        return self.get(
+            url=f'{APITestRoutes.OPERATIONS}/{operation_id}'
+        )
+
+    def get_operations_api(self, query: GetOperationsQueryTestSchema) -> Response:
+        return self.get(
+            url=APITestRoutes.OPERATIONS,
+            params=query.model_dump(by_alias=True, exclude_none=True),
+        )
+
+    def get_operation(self, operation_id: UUID) -> GetOperationResponseTestSchema:
+        response = self.get_operation_api(operation_id)
+        response.raise_for_status()
+        return GetOperationResponseTestSchema.model_validate_json(response.text)
+
+    def get_operations(self, user_id: UUID,
+                       card_id: UUID | None = None,
+                       account_id: UUID = None) -> GetOperationsResponseTestSchema:
+        query = GetOperationsQueryTestSchema(user_id=user_id, card_id=card_id, account_id=account_id)
+        response = self.get_operations_api(query)
+        response.raise_for_status()
+        return GetOperationsResponseTestSchema.model_validate_json(response.text)
+
+
+def build_operations_http_test_client() -> OperationsHttpTestClient:
+    client = build_http_test_client(
+        logger=get_test_logger("OPERATIONS_HTTP_TEST_CLIENT"),
+        config=test_settings.operations_http_client,
+    )
+    return OperationsHttpTestClient(client=client)
